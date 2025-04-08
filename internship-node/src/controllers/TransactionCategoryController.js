7a3f81fd52e25001e13a0d0024c63bac1b328653
vrajp2809 <128172585+vrajp2809@users.noreply.github.com>
2025-04-08 14:38:01 +0530

    transaction category update

--- internship-node/src/controllers/TransactionCategoryController.js
+++ internship-node/src/controllers/TransactionCategoryController.js
@@ -20,30 +20,6 @@ const getAllTranCategory = async(req,res)=>{
 
 
 
-// const addTranCategory = async(req,res)=>{
-
-//     try{
-//         const addedTransactionCategory = await TransactionCategoryModel.create(req.body);
-
-//         res.status(200).json({
-//             message:'Transaction Category Added successfully',
-//             data:addedTransactionCategory,
-//         })
-
-//     }catch(error)
-//     {
-//         res.status(500).json({
-//             message:'Error in adding Transaction Category',
-//             error:error.message,
-//         })
-//     }
-// }
-
-
-
-
-
-
 const addTranCategory = async (req, res) => {
     try {
         const { category, TranTypeId } = req.body;
